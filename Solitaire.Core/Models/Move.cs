daf582c1db86a3341b4e554c7120f48382b0f8e4
27SaidH <157550542+27SaidH@users.noreply.github.com>
2026-04-13 23:51:40 -0500

    Create Move.cs

---
+++ Solitaire.Core/Models/Move.cs
@@ -0,0 +1,10 @@
+namespace cs449sprint2.Models
+{
+    public class Move
+    {
+        public int FromRow { get; set; }
+        public int FromCol { get; set; }
+        public int ToRow { get; set; }
+        public int ToCol { get; set; }
+    }
+}
